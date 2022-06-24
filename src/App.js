70aa1f19161bfca2bb2c9fbb19a9a7417f6f559f
thatvictor7 <montoya.victor7@gmail.com>
2022-06-23 23:32:30 -0600

    added responsive navbar component from Boostrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './styling/App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar  from './components/navbar.jsx';
 
 function App() {
   return (
     <div className="App flex-center">
-      app
+      <Navbar />
+      
     </div>
   );
 }
