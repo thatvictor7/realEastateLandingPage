737dd809357ba8475006690cfe1e6acb3f8f1e92
thatvictor7 <montoya.victor7@gmail.com>
2022-06-29 23:20:09 -0600

    made components responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './styling/App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar  from './components/navbar.jsx';
+import Intro from './components/intro.jsx'
 
 function App() {
   return (
     <div className="App flex-center">
       <Navbar />
-      
+      <Intro />
     </div>
   );
 }
---
+++ src/components/intro.jsx
@@ -0,0 +1,10 @@
+import '../styling/intro.scss'
+
+export default function intro() {
+    return(
+        <div id='intro-container' className='flex-center'>
+            <div id='description-section' className='flex-center'>a</div>
+            <div id='description-image' className='flex-center'></div>
+        </div>
+    )
+}
