43f6ae01bbe7cdb8bad3828d7e0967fc999f0a95
thatvictor7 <montoya.victor7@gmail.com>
2022-07-01 01:36:33 -0600

    added function to highlight text in title

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,9 +1,22 @@
 import '../styling/intro.scss'
+import Card from './textCard.jsx'
 
 export default function intro() {
+
+    const text = `Lorem ipsum dolor sit amet, consectetur 
+    adipiscing elit, sed do eiusmod tempor 
+    incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation 
+    ullamco laboris nisi ut aliquip ex ea commodo 
+    consequat.`
+    const button = `Get Started >`
+    const title = [`Now It's Easier Than Ever To Find Your Home `,[6,7]]
+
     return(
         <div id='intro-container' className='flex-center'>
-            <div id='description-section' className='flex-center'>a</div>
+            <div id='description-section' className='flex-center'>
+                <Card textSection={text} button={button} title={title} />
+            </div>
             <div id='description-image' className='flex-center'></div>
         </div>
     )
---
+++ src/components/textCard.jsx
@@ -0,0 +1,58 @@
+import '../styling/textCard.scss'
+import Button from 'react-bootstrap/Button'
+
+function cardTitleColorize(text,colorPositions) {
+    const textArray = text.split(' ')
+
+    const startPosition = colorPositions[0] - 1
+    const endPosition = colorPositions[1] - 1
+
+    let regularText = []
+    let colorizedText = []
+    let additionalText = []
+
+    let res = ''
+
+    let test = <p></p>
+
+    textArray.map((e,i) => {
+        if (i < startPosition) {
+            regularText.push(e)
+        }
+        else if (i >= startPosition && i <= endPosition) {
+            // console.log('greater than: ' + i);
+            colorizedText.push(e)
+        }
+        else {
+            additionalText.push(e)
+        }
+    
+    })
+    // console.log(typeof regularText.join(' '));
+    res = res + regularText.join(' ')
+    // test += <p>fsdf</p>
+    // console.log(test);
+    return(
+        <div>
+            <p>{regularText.join(' ')} <span id='emphasized'>{colorizedText.join(' ')}</span> </p>  
+        </div>
+    )
+}
+
+export default function TextCard(props) {
+
+    // const button = props.button
+    const textSection = props.textSection
+    const buttonText = props.button
+    const titleSection = props.title
+    
+    
+    return(
+        <div id='card-container'>
+            <h4 id='top-card'>Top Component</h4>
+            <h1>{cardTitleColorize(titleSection[0], titleSection[1])}</h1>
+            <p>{textSection}</p>
+            <Button >{buttonText}</Button>
+        </div>
+    )
+}
