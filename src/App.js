799b9ee0c10c203eab529c1bbf09edf526b43cba
thatvictor7 <montoya.victor7@gmail.com>
2022-07-07 21:56:27 -0600

    added gallery section with carousel.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './styling/App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar  from './components/navbar.jsx';
+import Navbar  from './components/navbar.jsx'
 import Intro from './components/intro.jsx'
+import Gallery from './components/gallery.jsx'
 
 function App() {
   return (
     <div className="App flex-center">
       <Navbar />
       <Intro />
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/gallery.jsx
@@ -0,0 +1,52 @@
+import '../styling/gallery.scss'
+import Card from './textCard.jsx'
+import Carousel from 'react-bootstrap/Carousel'
+
+const title1 = [`Now It's Easier Than Ever To Find Your Home `, [4, 6]]
+
+export default function gallery(props) {
+    return (
+        <div id='gallery-container' className='flex-center'>
+            <div id='top-section-container'>
+                <Card textSection='aaa' button='Go Shopping >' title1={title1} title2='sdfsdf' />
+                <Carousel>
+                    <Carousel.Item interval={1000}>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=First slide&bg=373940"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item interval={500}>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Second slide&bg=282c34"
+                            alt="Second slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                            alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </div>
+    )
+}
