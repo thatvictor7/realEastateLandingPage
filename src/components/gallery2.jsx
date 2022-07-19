d76cdba57081fab71f7f86d1fbf9daa10b217f7d
thatvictor7 <montoya.victor7@gmail.com>
2022-07-19 15:36:26 -0600

    customized right section of gallery

--- src/components/gallery2.jsx
+++ src/components/gallery2.jsx
@@ -1,5 +1,6 @@
 import '../styling/gallery.scss'
-import Card from './textCard'
+import Card from 'react-bootstrap/Card'
+import CustomCard from './textCard'
 import SidewayCard from './sidewayCard.jsx'
 import DoorImg from '../assets/door.jpg'
 import Home3 from '../assets/home3.jpg'
@@ -11,9 +12,10 @@ Lacus laoreet non curabitur gravida. Scelerisque eleifend donec pretium vulputat
 Hendrerit gravida rutrum quisque non tellus orci. Enim eu turpis egestas pretium aenean pharetra magna ac placerat. In tellus integer feugiat scelerisque varius. Neque convallis a cras semper auctor neque vitae. 
 
 Id semper risus in hendrerit gravida.`
+const videoCardText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
 
 export default function Gallery2(props) {
-    return(
+    return (
         <div className='flex-center' id='bottom-section-container'>
             <div id='left-section-gallery' className='full-width flex-center'>
                 {/* <div className='section-container-gallery flex-center'>
@@ -24,7 +26,13 @@ export default function Gallery2(props) {
                     <SidewayCard text1='Ultimo Eventu Ostendunt' text2='MCCXL, Aureum Statum Londinensi' orientation='ru' />
                     <img src={Home3} className='section-image' />
                 </div> */}
-                <div>a</div>
+                <div>
+                    {/* <SidewayCard text1='Ultimo Eventu Ostendunt' text2='MCCXL, Aureum Statum Londinensi' orientation='lu' />
+                <SidewayCard text1='Ultimo Eventu Ostendunt' text2='MCCXL, Aureum Statum Londinensi' orientation='lu' /> */}
+                    <div className='side-text'>
+                        This is just a test
+                    </div>
+                </div>
                 <div id='image-conatiner' className='flex-center'>
                     {/* <img src={DoorImg} className='section-image' />
                     <img src={Home3} className='section-image' /> */}
@@ -33,9 +41,16 @@ export default function Gallery2(props) {
                 </div>
             </div>
             <div id='mid-section-gallery' className='full-width'>
-                <Card width='100%' textSection={text} title1={[`De Nobis`, [2,2   ]]} />
+                <CustomCard width='100%' textSection={text} title1={[`De Nobis`, [2, 2]]} />
             </div>
-            <div id='right-section-gallery' className='full-width'>column3</div>
+            <div id='right-section-gallery' className='full-width'>
+                <div id='video-section'>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src='https://via.placeholder.com/330x500/000fff? Text=Video'/>
+                    </Card>
+                </div>
+                <CustomCard textSection={videoCardText} title1={[`This is a test as well`, [4, 5]]} />
+            </div>
         </div>
     )
 }
