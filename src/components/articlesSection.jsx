0f9892ae98a5f32b689b2586d9a2ad88ae4697cf
thatvictor7 <montoya.victor7@gmail.com>
2022-07-22 00:32:36 -0600

    modified logo, created grid for articles

--- src/components/articlesSection.jsx
+++ src/components/articlesSection.jsx
@@ -1,9 +1,21 @@
 import '../styling/articlesSection.scss'
+import CustomCard from './textCard'
+
+const text = `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`
 
 export default function articleSection() {
     return(
-        <div id='articles-container'>
-            articles
+        <div id='articles-container' className='flex-center'>
+            <div id='article-card'>
+                <CustomCard width='100%' textSection={text} title1={[`De Nobis`, [2, 2]]} />
+            </div>
+            <div id='articles' className='flex-center'>
+                <div style={{width: '50%', backgroundColor: 'greenyellow'}}>a</div>
+                <div style={{width: '50%', backgroundColor: 'greenyellow'}}>b</div>
+                <div style={{width: '50%', backgroundColor: 'greenyellow'}}>c</div>
+                <div style={{width: '25%', backgroundColor: 'greenyellow'}}>f</div>
+                <div style={{width: '25%', backgroundColor: 'greenyellow'}}>g</div>
+            </div>
         </div>
     )
 }
