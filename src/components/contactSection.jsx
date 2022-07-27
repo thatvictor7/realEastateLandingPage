f795900653ddf5c508e8f0be2c24c3961d5e6105
thatvictor7 <montoya.victor7@gmail.com>
2022-07-27 00:08:13 -0600

    added icons, added social media section

--- src/components/contactSection.jsx
+++ src/components/contactSection.jsx
@@ -1,5 +1,6 @@
 import '../styling/contactSection.scss'
 import homeImage from '../assets/home4.jpg'
+import { Instagram, Twitter, Facebook, Youtube } from 'react-bootstrap-icons'
 
 export default function Contact(params) {
     return(
@@ -7,8 +8,23 @@ export default function Contact(params) {
             <div id='contact-image' className='flex-center' style={{backgroundImage: `url(${homeImage})`}}>
                 first
             </div>
-            <div>
-                second
+            <div id='social-container' className='flex-center'>
+                <div className='social-element'>
+                    <Instagram />
+                    <span>Instagram</span>
+                </div>
+                <div className='social-element'>
+                    <Twitter />
+                    <span>Twitter</span>
+                </div>
+                <div className='social-element'>
+                    <Facebook />
+                    <span>Facebook</span>
+                </div>
+                <div className='social-element'>
+                    <Youtube />
+                    <span>YouTube</span>
+                </div>
             </div>
         </div>
     )
